e9980429be04969918bad7fcb056eeb3ad73b185
gaaroth <marcello.garini@gmail.com>
2016-10-19 11:26:22 +0200

    Add modify form + code review

--- src/main/java/org/gaaroth/leeroyjenkins/MyUI.java
+++ src/main/java/org/gaaroth/leeroyjenkins/MyUI.java
@@ -7,13 +7,19 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.BeanBinder;
+import com.vaadin.data.util.converter.StringToLongConverter;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -28,32 +34,69 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
+        VerticalLayout layoutRoot = new VerticalLayout();
         
-        final Label title = new Label("BOOK GRID");
+        // TABLE
+        Label titleGrid = new Label("BOOK GRID");
+        titleGrid.addStyleName(ValoTheme.LABEL_COLORED);
+        titleGrid.addStyleName(ValoTheme.LABEL_H2);
         
         Grid<Book> grid = new Grid<>();
 		grid.setSizeFull();
-		grid.addColumn("ID", t -> {
-			return t.getId().toString();
-		});
-		grid.addColumn("Title", t -> {
-			return t.getTitle();
-		});
-		grid.addColumn("Description", t -> {
-			return t.getDescription();
-		});
+		grid.addColumn("ID", book -> book.getId().toString());
+		grid.addColumn("Title", book -> book.getTitle());
+		grid.addColumn("Description", book -> book.getDescription());
 
-        Button button = new Button("Reload");
-        button.addClickListener( e -> {
-            grid.setItems(generateRandomBooks());
+        Button reload = new Button("Load new data");
+        reload.addClickListener(event -> grid.setItems(generateRandomBooks()));
+        
+        //FORM
+        Label titleForm = new Label("BOOK FORM");
+        titleForm.addStyleName(ValoTheme.LABEL_COLORED);
+        titleForm.addStyleName(ValoTheme.LABEL_H2);
+        
+        TextField idField = new TextField();
+        idField.setEnabled(false);
+        TextField titleField = new TextField();
+        TextField descriptionField = new TextField();
+        Button refresh = new Button("Refresh grid");
+        
+        final BeanBinder<Book> binder = new BeanBinder<>(Book.class);
+        binder.forField(idField).withConverter(new StringToLongConverter("Not a valid ID")).bind("id");
+        binder.forField(titleField).withValidator(value -> value.length() > 0, "Must be compiled").bind("title");
+        binder.forField(descriptionField).bind("description");
+        binder.setValidationStatusHandler(validationStatus -> refresh.setEnabled(validationStatus.isOk()));
+     
+        titleField.addValueChangeListener(event -> binder.validate());
+        grid.addItemClickListener(item -> binder.bind(item.getItem()));
+        refresh.addClickListener(event -> {
+        	grid.getDataSource().refreshAll();
         });
         
-        layout.addComponents(title, grid, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
+        final HorizontalLayout layoutOverTable = new HorizontalLayout();
+        layoutOverTable.setWidth("100%");
+        layoutOverTable.addComponents(titleGrid, reload);
+        layoutOverTable.setComponentAlignment(reload, Alignment.MIDDLE_RIGHT);
+        layoutOverTable.setSpacing(true);
         
-        setContent(layout);
+        final HorizontalLayout layoutBinder = new HorizontalLayout();
+        layoutBinder.setWidth("100%");
+        layoutBinder.addComponents(idField, titleField, descriptionField, refresh);
+        layoutBinder.setComponentAlignment(refresh, Alignment.MIDDLE_RIGHT);
+        layoutBinder.setExpandRatio(refresh, 1);
+        layoutBinder.setSpacing(true);
+        
+        final VerticalLayout layoutForm = new VerticalLayout();
+        layoutForm.setWidth("100%");
+        layoutForm.addComponents(titleForm, layoutBinder);
+        layoutForm.setSpacing(true);
+        
+        layoutRoot.addComponents(layoutOverTable, grid, layoutForm);
+        layoutRoot.setMargin(true);
+        layoutRoot.setSpacing(true);
+        
+        setContent(layoutRoot);
+
     }
     
     private List<Book> generateRandomBooks() {
